c39b641c7398aea1bc094b41af4f2c09f3fef666
M Nouman Berlas <noumanberlas@gmail.com>
2023-05-08 12:06:29 -0400

    First Commit

---
+++ Constants.js
@@ -0,0 +1,8 @@
+import { Dimensions } from 'react-native';
+
+export default Constants = {
+    MAX_WIDTH: Dimensions.get("screen").width,
+    MAX_HEIGHT: Dimensions.get("screen").height,
+    GAP_SIZE: 400, // gap between the two parts of the pipe
+    PIPE_WIDTH: 50 // width of the pipe
+}
